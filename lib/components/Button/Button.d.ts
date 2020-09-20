d42cec7d9a9ac9ccb6cb09d7439ff6de4a970449
Yuri S <yuri.sakharov@clarizen.com>
2020-09-20 14:43:38 +0300

    Pushed lib folder

---
+++ lib/components/Button/Button.d.ts
@@ -0,0 +1,11 @@
+import React from "react";
+export declare type Props = {
+    children: React.ReactNode;
+    onClick?: () => void;
+    type?: string;
+    disabled?: boolean;
+    dataTestId?: string;
+} & React.ButtonHTMLAttributes<HTMLButtonElement>;
+declare const _default: React.NamedExoticComponent<Props>;
+export default _default;
+//# sourceMappingURL=Button.d.ts.map
